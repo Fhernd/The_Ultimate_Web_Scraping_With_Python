9ca3fc933de4096a5ea952d8da4135343a1ee88e
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-05 06:42:09 -0500

    Obtención del precio y la puntuación del producto

--- ch05_parsing_and_extraction/exercise5801_books_data.py
+++ ch05_parsing_and_extraction/exercise5801_books_data.py
@@ -11,5 +11,30 @@ soup = BeautifulSoup(content, 'html.parser')
 books = soup.find_all('article', class_='product_pod')
 print('Books:', len(books))
 
+data = []
+
+stars = {
+    'One': 1,
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5
+}
+
 for b in books:
-    pass
+    book = {}
+
+    book['title'] = b.find('h3').find('a').get_text()
+
+    rating = b.find('p', class_='star-rating')
+    book['rating'] = stars[rating.attrs['class'][1]]
+
+    price = b.find('div', class_='price_color')
+    book['price'] = b.get_text()
+
+    data.append(book)
+
+print()
+
+for d in data:
+    print(d)

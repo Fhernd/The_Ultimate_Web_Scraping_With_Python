cfbd2d74e0cb4b632b6edbaf2164e9ef52413bf6
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-06 06:39:44 -0500

    Representación de datos de libros en un DataFrame de pandas

---
+++ ch05_parsing_and_extraction/demo51001_pandas.py
@@ -0,0 +1,68 @@
+import re
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+url = 'https://books.toscrape.com'
+response = requests.get(url)
+
+content = response.text
+
+soup = BeautifulSoup(content, 'html.parser')
+
+books = soup.find_all('article', class_='product_pod')
+print('Books:', len(books))
+
+data = []
+
+stars = {
+    'One': 1,
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5
+}
+
+pattern = r'\d+\.\d+'
+
+for b in books:
+    book = {}
+
+    book_link = b.find('h3').find('a').attrs['href']
+    book_link = f'https://books.toscrape.com/{book_link}'
+    
+    response = requests.get(book_link)
+    content = response.text
+    book_page = BeautifulSoup(content, 'html.parser')
+    book_title = book_page.find('div', class_='product_main')
+    book_title = book_title.find('h1').get_text()
+    book['title'] = book_title
+
+    rating = b.find('p', class_='star-rating')
+    book['rating'] = stars[rating.attrs['class'][1]]
+
+    price = b.find('p', class_='price_color')
+    price = price.get_text()
+    price = re.search(pattern, price)
+    book['price'] = float(price.group())
+
+    data.append(book)
+
+print()
+
+df = pd.DataFrame(data)
+
+print(df)
+
+print()
+
+print(df.price.mean())
+
+print()
+
+print(df[df.price < 20])
+
+print()
+
+print(df[df.price < 20].title)

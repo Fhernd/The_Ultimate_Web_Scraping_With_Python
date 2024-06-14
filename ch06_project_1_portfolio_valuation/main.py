7679147b10f9b3c97ee9fecb25c8569cc17d36f1
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-13 19:09:28 -0500

    Extrae el precio de una acción

--- ch06_project_1_portfolio_valuation/main.py
+++ ch06_project_1_portfolio_valuation/main.py
@@ -2,6 +2,27 @@ from bs4 import BeautifulSoup
 import requests as r
 
 
+def get_price_information(ticker, exchange):
+    """
+    Get the price information of a stock from Yahoo Finance.
+
+    Parameters:
+    ticker (str): The ticker of the stock.
+    exchange (str): The exchange of the stock.
+
+    Returns:
+    str: The price of the stock.
+    """
+    
+    url = f"https://finance.yahoo.com/quote/{ticker}:{exchange}"
+    response = r.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    price = soup.find("div", {"data-last-price": True})
+
+    return price["data-last-price"]
+
+
 def main():
     pass
 

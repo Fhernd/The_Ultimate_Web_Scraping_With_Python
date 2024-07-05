ecc876ef4643f429d43820b592920f8529e49e72
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-05 10:37:34 -0500

    Firma para el método que realiza la extracción de datos desde Tim's Careers

--- ch07_api_hidden_gems/challenge01_open_positions_tims_careers.py
+++ ch07_api_hidden_gems/challenge01_open_positions_tims_careers.py
@@ -9,8 +9,21 @@ class JobPosition:
     requirements: str
 
 
-def main():
+def extract_job_positions(url, results, lat, lng, postal_code):
     pass
+
+def main():
+    """
+    Main function to extract job positions from Tim's Careers API.
+    """
+    lat = 29.784753
+    lng = -95.361416
+    postal_code = '77003'
+    results = 10
+    
+    url = f'https://api.higherme.com/classic/jobs?page=1&includes=location,location.company,location.externalServiceReferences&limit=24&filters[brand.id]=58bd9e7f472bd&filters[lat]={lat}&filters[lng]={lng}&filters[distance]=2290599.375&sort[distance]=asc'
+    
+    extract_job_positions(url, results, lat, lng, postal_code)
 
 
 if __name__ == '__main__':

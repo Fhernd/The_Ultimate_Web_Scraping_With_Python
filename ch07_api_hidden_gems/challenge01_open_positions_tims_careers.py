5da086c210e270174dda830c9ee16553c304ff3e
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-05 10:31:05 -0500

    Clase de datos que representa un trabajo

---
+++ ch07_api_hidden_gems/challenge01_open_positions_tims_careers.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class JobPosition:
+    title: str
+    work_mode: str
+    distance: float
+    requirements: str
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
